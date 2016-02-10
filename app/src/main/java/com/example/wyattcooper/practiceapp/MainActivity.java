5f189a13716aef82819a0ed7dbf0c8436b286dbc
Wyatt Cooper <wcooper17@cmc.edu>
2016-02-10 15:42:01 -0800

    Practicing with Firebase

--- app/src/main/java/com/example/wyattcooper/practiceapp/MainActivity.java
+++ app/src/main/java/com/example/wyattcooper/practiceapp/MainActivity.java
@@ -7,6 +7,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.NumberPicker;
 import java.util.Random;
+import com.firebase.client.Firebase;
+import com.firebase.client.ValueEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.FirebaseError;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
@@ -16,8 +20,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+
+        final Firebase myFirebaseRef = new Firebase("https://dazzling-inferno-9759.firebaseio.com/");
+        myFirebaseRef.child("message").setValue("Do you have data? You'll love Firebase.");
 
         textView = (TextView) findViewById(R.id.textView);
+
+        myFirebaseRef.child("message").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                String data = (String) snapshot.getValue();
+                textView.setText(data);
+            }
+            @Override public void onCancelled(FirebaseError error) { }
+        });
+
+
         np = (NumberPicker) findViewById(R.id.numberPicker);
         Random rand = new Random();
         final int randomVal = rand.nextInt(20);
@@ -35,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
                     if (newVal == randomVal) {
                         textView.setText("You got it");
+                        myFirebaseRef.child("values").setValue(newVal);
                     }
             }
         });
